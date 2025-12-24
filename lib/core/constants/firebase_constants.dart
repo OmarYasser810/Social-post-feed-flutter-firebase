bad05047832b81bbbcd7bd6a1c7916e8c459fa94
OmarYasser810 <OmarYasser05810@gmail.com>
2025-12-24 17:56:25 +0200

    create constants folder

---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,12 @@
+class FirebaseConstants {
+  // Collection names
+  static const usersCollection = 'users';
+  static const postsCollection = 'posts';
+  static const commentsCollection = 'comments';
+  static const likesCollection = 'likes';
+  static const followsCollection = 'follows';
+  
+  // Sub-collection names
+  static const followingSubCollection = 'following';
+  static const followersSubCollection = 'followers';
+}
